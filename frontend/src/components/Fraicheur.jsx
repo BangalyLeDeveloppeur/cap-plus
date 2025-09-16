af2a30fd7c15fb1c4622fba7b529e823d547b4df
BangalyLeDeveloppeur <condeBangaly580@gmail.com>
2025-09-16 10:00:02 +0200

    integration des pour point pour le slied d'accueil

--- frontend/src/components/Fraicheur.jsx
+++ frontend/src/components/Fraicheur.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-import bassinPiscicole from "../assets/images/bassin Piscicole.webp";
+import bassinPiscicole from "../assets/images/bassinPiscicole.webp";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircle as faSolidCircle } from "@fortawesome/free-solid-svg-icons";
+import { faCircle as faRegularCircle } from "@fortawesome/free-regular-svg-icons";
 
 const Fraicheur = () => {
   return (
@@ -11,13 +14,20 @@ const Fraicheur = () => {
           alt="Bassin piscicole"
         />
       </div>
+
       <div className="savourer">
         <p>
           Savourez la fraîcheur de nos poissons, élevés avec soin dans nos
           bassins
         </p>
         <button className="savoirPlus">En savoir plus</button>
+      <div className="fac-circle">
+        <FontAwesomeIcon icon={faSolidCircle} />
+        <FontAwesomeIcon icon={faRegularCircle} />
+        <FontAwesomeIcon icon={faRegularCircle} />
       </div>
+      </div>
+
     </div>
   );
 };
