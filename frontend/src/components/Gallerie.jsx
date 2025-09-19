8b32219c708cb42b47a19854cab101b1e13c7609
BangalyLeDeveloppeur <condeBangaly580@gmail.com>
2025-09-19 18:49:51 +0200

    styles de la rubrique galerie

--- frontend/src/components/Gallerie.jsx
+++ frontend/src/components/Gallerie.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Gallerie = () => {
   return (
-    <section>
+    <section className="offres">
       <h1 className="engagement-titre">Galerie</h1>
       <div className="bar-souligne-galerie"></div>
       <div className="Galerie"></div>
