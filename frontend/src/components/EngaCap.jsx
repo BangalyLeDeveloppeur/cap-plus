5768b4ebc89e6d88b0dcedf5da9094ab87ccada0
BangalyLeDeveloppeur <condeBangaly580@gmail.com>
2025-09-19 17:54:28 +0200

    style de la rubrique notre engagement

--- frontend/src/components/EngaCap.jsx
+++ frontend/src/components/EngaCap.jsx
@@ -5,6 +5,9 @@ const EngaCap = () => {
     <section className="engagement">
       <h1 className="engagement-titre">Notre Engagement</h1>
       <div className="bar-souligne">
+
+      </div>
+      <div className="">
         <p className="engagement-paragraphe">
           Nous garantissons un élevage durable et respectueux de
           l’environnement, offrant des poissons sains et nourris avec soin,
