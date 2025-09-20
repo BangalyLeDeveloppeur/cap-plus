9ce9393486617ef6ea433930d7965f3ae534f283
BangalyLeDeveloppeur <condeBangaly580@gmail.com>
2025-09-20 09:52:01 +0200

    style de la rubrique pourquoi nous choisir

--- frontend/src/components/Choisir.jsx
+++ frontend/src/components/Choisir.jsx
@@ -3,13 +3,15 @@ import React from "react";
 const Choisir = () => {
   return (
     <div>
-      <section>
-        <h1>Pourquoi nous choisir ?</h1>
-        <div className="bar"></div>
+      <section className="offres">
+        <h1 className="engagement-titre">Pourquoi nous choisir ?</h1>
+        <div className="bar-souligne-choi"></div>
         <div className="choisi-paragraphe">
+          <p>
           Parce que nous produisons la qualité, durabilité et confiance. Nos
           poissons sont sains, savoureux et élevés avec soin pour répondre aux
           besoins des familles, des marchés et de la restauration.
+          </p>
         </div>
       </section>
     </div>
