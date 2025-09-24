ad69e4cb4d81c1c78cd3d909af3ec9b45dfe14ef
BangalyLeDeveloppeur <condeBangaly580@gmail.com>
2025-09-24 09:40:53 +0200

    style et le code get des données de table galerie

--- frontend/src/components/Gallerie.jsx
+++ frontend/src/components/Gallerie.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
 const Gallerie = () => {
+  const [tablegal, setTableGal] = useState([]);
+
+  const getGalerie = async () => {
+    const response = await axios.get("http://localhost:5000/api/galerie");
+    setTableGal(response.data);
+  };
+  getGalerie();
+
   return (
     <section className="offres">
       <h1 className="engagement-titre">Galerie</h1>
       <div className="bar-souligne-galerie"></div>
-      <div className="Galerie"></div>
+      
+        {tablegal.slice(0,1).map((dg) => (
+          <div key={dg.id} className="galerie">
+            <h2>{dg.titre}</h2>
+            <p>{dg.desription}</p>
+            <img src={`http://localhost:5000${dg.image_url}`} alt="dg.titre" />
+           
+          </div>
+        ))}
+
+      
+      <button className="savoirPlusGal">En savoir plus</button>
     </section>
   );
 };
--- frontend/src/pages/AdminTableGalerie.jsx
+++ frontend/src/pages/AdminTableGalerie.jsx
@@ -1,23 +1,103 @@
 import axios from "axios";
-import React from "react";
 import { useState, useEffect } from "react";
 
 const AdminTableGalerie = () => {
   const [tablegalerie, setTableGalerie] = useState([]);
   const [titre, setTitre] = useState("");
   const [description, setDescription] = useState("");
+  const [message, setMessage] = useState("");
   const [image, setImage] = useState(null);
-
-  const fetchImageGalerie = async () => {
-    const sql = await axios("http://localhost:5000/api/galerie");
-    return sql.data;
-  };
 
   useEffect(() => {
     fetchImageGalerie();
   }, []);
 
-  return <div></div>;
+  const fetchImageGalerie = async () => {
+    const sql = await axios("http://localhost:5000/api/galerie");
+    setTableGalerie(sql.data);
+  };
+  console.log(tablegalerie);
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!image) {
+      setMessage("Rien n'a été ajouté dans la table galerie");
+    }
+
+    ///récupération des données dans le formulaire ////
+    const formData = new FormData();
+    formData.append("titre", titre);
+    formData.append("description", description);
+    formData.append("image", image);
+    ///console.log(formData)
+
+    try {
+      await axios.post("http://localhost:5000/api/galerie", formData, {
+        headers: { "Content-type": "multipart/form-data" },
+      });
+      setMessage("Galerie ajoutée avec succès");
+      setTitre("");
+      setDescription("");
+      setImage(null);
+      fetchImageGalerie();
+    } catch (error) {
+      console.log("Ajout échoué", error);
+      setMessage("❌ Erreur lors de l'upload");
+    }
+  };
+
+  return <div>
+     <h2>Administration de la table galerie</h2>
+
+      {message && <p>{message}</p>}
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Titre :</label>
+          <input
+            type="text"
+            value={titre}
+            onChange={(e) => setTitre(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Description :</label>
+          <input
+            type="text"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Image :</label>
+          <input
+            type="file"
+            onChange={(e) => setImage(e.target.files[0])}
+            required
+          />
+        </div>
+        <button type="submit">Ajouter</button>
+      </form>
+
+      {/* Affichage des offres */}
+      <h3>Liste galerie</h3>
+      <ul>
+        {tablegalerie.map((gal) => (
+          <li key={gal.id}>
+            <h4>{gal.titre}</h4>
+            <p>{gal.description}</p>
+            <img
+              src={`http://localhost:5000${gal.image_url}`}
+              alt={gal.titre}
+              width="150"
+            />
+          </li>
+        ))}
+      </ul>
+  </div>;
 };
 
 export default AdminTableGalerie;
