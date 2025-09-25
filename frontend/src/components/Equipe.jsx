fdadcf7ffb52d5902a2614d1b1d1c9c1ef7ad462
BangalyLeDeveloppeur <condeBangaly580@gmail.com>
2025-09-25 13:32:54 +0200

    style apropos

--- frontend/src/components/Equipe.jsx
+++ frontend/src/components/Equipe.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Equipe = () => {
   return (
-    <div>
+    <div className="engagement">
       <div className="passion-titre">
-        <h1>Equipe</h1>
-        <div className="bar-souligne"></div>
+        <h1>Notre équipe</h1>
+        <div className="bar-souligne pton"></div>
         <div className="equipe-contener">
           <span>notre équipe</span>
         </div>
--- frontend/src/components/Passion.jsx
+++ frontend/src/components/Passion.jsx
@@ -1,31 +1,36 @@
 import React from "react";
-import Main from "../assets/images/main.png"
+import Main from "../assets/images/main.png";
 
 const Passion = () => {
   return (
-    <div>
+    <div className="engagement">
       <div className="passion-titre">
         <h1>Apropos de nous</h1>
-        <div className="bar-souligne"></div>
+        <div className="bar-souligne pton">
+
+        </div>
       </div>
       <div className="passion-paragraphe">
-        <p>
-          Nous, Bangaly et Nina, avons transformé notre passion en une véritable
-          vocation. Animés par l’amour de l’agriculture et de la pisciculture,
-          nous avons fait de l’élevage du tilapia, du tilapia rouge et du
-          silure, ainsi que de la culture maraîchère, bien plus qu’une activité
-          : un projet de vie. De cette vision est née CAP-PLUS, une entreprise
-          d’agrobusiness bâtie sur le savoir-faire, l’innovation et un profond
-          respect de l’environnement. Chaque jour, nous nous engageons à
-          produire des aliments sains, frais et de qualité, tout en créant de la
-          valeur pour nos communautés. Notre ambition est claire : nourrir
-          aujourd’hui de manière responsable, tout en préservant les ressources
-          pour demain et en contribuant activement au développement local et à
-          la sécurité alimentaire.
-        </p>
-        <img src={Main}/>
+        <div className="apropos-paragraphe">
+          <p>
+            Nous, Bangaly et Nina, avons transformé notre passion en une
+            véritable vocation. Animés par l’amour de l’agriculture et de la
+            pisciculture, nous avons fait de l’élevage du tilapia, du tilapia
+            rouge et du silure, ainsi que de la culture maraîchère, bien plus
+            qu’une activité: un projet de vie. <br />
+            De cette vision est née CAP-PLUS, une entreprise d’agrobusiness
+            bâtie sur le savoir-faire, l’innovation et un profond respect de
+            l’environnement. <br /> Chaque jour, nous nous engageons à produire
+            des aliments sains, frais et de qualité, tout en créant de la valeur
+            pour nos communautés. Notre ambition est claire : nourrir
+            aujourd’hui de manière responsable, tout en préservant les
+            ressources pour demain et en contribuant activement au développement
+            local et à la sécurité alimentaire.
+          </p>
+        </div>
+        <img src={Main} />
       </div>
-      <button className="savoirPlusGal">En savoir plus</button>
+      <button className="savoirPlusGal passion">En savoir plus</button>
     </div>
   );
 };
