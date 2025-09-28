15e90604d8c26636f81b5ef66dc76c0af5e08328
BangalyLeDeveloppeur <condeBangaly580@gmail.com>
2025-09-28 08:35:10 +0200

    correction du beg du slid

--- frontend/src/components/Gallerie.jsx
+++ frontend/src/components/Gallerie.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Gallerie = () => {
   const [tablegal, setTableGal] = useState([]);
@@ -8,23 +8,23 @@ const Gallerie = () => {
     const response = await axios.get("http://localhost:5000/api/galerie");
     setTableGal(response.data);
   };
-  getGalerie();
 
+  useEffect(() => {
+    getGalerie();
+  }, []);
   return (
     <section className="offres">
       <h1 className="engagement-titre">Galerie</h1>
       <div className="bar-souligne-galerie"></div>
-      
-        {tablegal.slice(0,1).map((dg) => (
-          <div key={dg.id} className="galerie">
-            <h2>{dg.titre}</h2>
-            <p>{dg.desription}</p>
-            <img src={`http://localhost:5000${dg.image_url}`} alt="dg.titre" />
-           
-          </div>
-        ))}
 
-      
+      {tablegal.slice(0, 1).map((dg) => (
+        <div key={dg.id} className="galerie">
+          <h2>{dg.titre}</h2>
+          <p>{dg.desription}</p>
+          <img src={`http://localhost:5000${dg.image_url}`} alt="dg.titre" />
+        </div>
+      ))}
+
       <button className="savoirPlusGal">En savoir plus</button>
     </section>
   );
